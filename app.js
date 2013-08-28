4824a5c26d543f94318a80b72820e04c9b205aad
John Lium <John.Lium@aunalytics.com>
2013-08-28 16:58:48 -0400

    Updated to use async

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
 var Browser = require("zombie");
 var assert = require("assert");
+var async = require("async");
 
 var browser = new Browser();
 
 var url = "http://www.glycemicindex.com/foodSearch.php?ak=list&food_name_search_type=cn&food_name=&gi_search_type=lte&gi=&gl_search_type=lte&gl=&country=&product_category=&lop=OR&find=Find+Records&page=1"
 
-var css_selector = "html body.search div table#table1 tbody tr td table#table2 tbody tr td table#table39 tbody tr td table#table42 tbody tr td table#table43 tbody tr td table tbody";
+var css_selector = "html body.search div table#table1 tbody tr td table#table2 tbody tr td table#table39 tbody tr td table#table42 tbody tr td table#table43 tbody tr td table tbody tr";
 
-browser.visit(url).
-  then(function() {
-    var table = browser.query(css_selector);
-    var rows = browser.queryAll("tr", table);
+scrapePage(url, null);
 
-    for(var r in rows) {
-      var fields = browser.queryAll("td", rows[r]);
+function scrapePage(url, next) {
+  browser.visit(url).
+    then(function() {
+      var rows = browser.queryAll(css_selector);
+      rows.shift();
+      async.map(rows, rowToStr, function(err, results) {
+        console.log( results.join("\n") );
+        console.log( results.length );
+      });
+    }).
+    fail(function(error) {
+      console.log(error);
+    });
+}
 
-      var text_array = []
-      for(var f in fields) {
-        var text = browser.text("p", fields[f]);
-        text_array.push(text);
-      }
-      console.log(text_array.join("|"));
-
-    }
-    console.log(rows.length);
-
-  }).
-  fail(function(error) {
-    console.log(error);
+function rowToStr(row, next) {
+  var fields = browser.queryAll("td", row);
+  async.map(fields, fieldToStr, function(err, results) {
+    next(null, results.join("|"));
   });
+}
 
+function fieldToStr(field, next) {
+  next(null, browser.text("p", field));
+}
