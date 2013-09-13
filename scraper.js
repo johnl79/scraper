2625e734d2758b1a6741156490669b122bec6544
John Lium <John.Lium@aunalytics.com>
2013-09-13 13:16:15 -0400

    Scaling back to test

--- scraper.js
+++ scraper.js
@@ -6,18 +6,24 @@ var _ = require("underscore");
 var base_url = "http://www.in.gov/dlgf/4931.htm";
 var download_css_path = "html body form#form1 div a#btnDownLoad"
 
-var browser = new Browser();
+var browser = new Browser({ debug: true });
 var letter = "a"
 console.error("Search: " + letter);
 browser.visit(base_url).
   then(function() {
+    console.log("Visited!");
+
+    /*
     browser
       .fill("Property Street Address", letter)
       .pressButton("Search", function() {
-        browser.clickLink("Download", function() {
+          console.log("Searched!");
+          browser.clickLink("Download", function() {
           console.log("What the what?");
         })
       })
+    */
+
   }).
   fail(function(error) {
     next(error, null);
