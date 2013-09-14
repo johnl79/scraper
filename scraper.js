e564b1259a66dda6c8929fc9b6beedc36ad1a0a7
John Lium <John.Lium@aunalytics.com>
2013-09-14 16:54:22 -0400

    Switching gears, now scraping the ID detail href
    Putting the csv download on the backburner, big timesink

--- scraper.js
+++ scraper.js
@@ -9,7 +9,25 @@ function debug(msg) {
 };
 
 var base_url = "http://www.in.gov/apps/dlgftax/Assessment/AssessmentSearch.aspx";
-var search_button_css = "html body form#form1 div table tbody tr td input#txtStreet";
+
+var assesment_year_css = "html body form#form1 div table tbody tr td select#ddlAssesYear";
+var assesment_years = ['2008', '2009', '2010', '2011', '2012']
+
+var county_code_css = "html body form#form1 div table tbody tr td select#ddlCounty";
+var county_codes = _.range(1,92).map(String);
+
+var property_type_css = "html body form#form1 div table tbody tr td select#ddlPropType";
+var property_types = ["1%", "2%", "3%", "4%", "5%", "6%", "7%", "8%"];
+
+var street_address_css = "html body form#form1 div table tbody tr td input#txtStreet"
+var street_addresses = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+var search_button_css = "html body form#form1 div table tbody tr td input#btnSearch";
+var download_link_css= "html body form#form1 div a#btnDownLoad"
+var pagnation_table_css = "html body form#form1 div div table#gvResults.datagrid tbody tr td table"
+
+var detail_link_css = "html body form#form1 div div table#gvResults.datagrid tbody tr td a"
+
 var letter = "a";
 var export_js = "var theForm = document.forms[0]; theForm.__EVENTTARGET = 'btnDownLoad'; theForm.__EVENTARGUMENT = ''; theForm.submit()";
 
@@ -20,28 +38,31 @@ Browser.visit(base_url, { debug: true, waitFor: 5000}, function(e, browser, stat
   // Handle prompts
   browser.onprompt(function(msg) {
     console.log("prompted");
+    console.log(msg);
   });
 
-  // Execute a search given the defined parameters
-  console.log("search: " + letter);
-  browser.fill(search_button_css, letter).pressButton("Search", function() {
-    console.log("search results rendered");
-
-    // Request download by running the code in export_js
-    console.log("requesting download");
-
-    // This is a synchronous function that returns the result. We have to
-    // wait on the response.
-    export_eval = browser.evaluate(export_js)
-
-    // The wait should be predicated on some function to look for the response,
-    // however, I currently have no idea where to look for it. Just calling the
-    // debugger for now.
-
-    browser.wait(function() {
-      console.log("download response");
-      debugger;
-    });
+  // Execute a search given the defined parameters, for now, print the detail ID for the first page of results.
+  var street_address = street_addresses[0]
+  var assesment_year = assesment_years[0]
+  var county_code = county_codes[0]
+  console.log(property_types);
+  var property_type = property_types[5]
+  browser
+    .select(assesment_year_css, assesment_year)
+    .select(county_code_css, county_code)
+    .select(property_type_css, property_type)
+    .fill(street_address_css, street_address)
+    .pressButton("Search", function() {
+      console.log("waiting for search results to render");
+      browser.wait(function() {
+        console.log("search results rendered");
+        var detail_links = browser.queryAll(detail_link_css);
+        for(d in _.range(0,9)) {
+          var id_number = detail_links[d]._attributes._nodes.href._childNodes[0].__nodeValue.split('?ID=')[1]
+          console.log( [assesment_year, county_code, property_type, street_address, id_number].join(','));
+        }
+        debugger;
+      });
 
   });
 
