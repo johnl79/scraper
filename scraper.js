b24d5588bf91297fafc3d4c9df2ff856093bc7c2
John Lium <John.Lium@aunalytics.com>
2013-09-16 08:49:11 -0400

    Working scraper for detail IDs. Pretty significant memory leak.

--- scraper.js
+++ scraper.js
@@ -4,23 +4,72 @@ var async = require("async");
 var _ = require("underscore");
 
 function debug(msg) {
-  console.log(msg);
+  console.error(msg);
   debugger;
+};
+
+// Execute a search given the passed parameters, for now, print the detail ID for the first page of results.
+function doSearch(options) {
+
+  console.error("new search: " + JSON.stringify(options));
+  var street_address = options['street_address'];
+  var assesment_year = options['assesment_year'];
+  var county_code = options['county_code'];
+  var property_type = options['property_type'];
+  delete browser;
+  var browser = new Browser({debug: false, windowName: "Firefox", runScripts: false});
+  browser.visit(base_url, function() {
+    try {
+      checks = [browser.query(assesment_year_css), browser.query(county_code_css), browser.query(property_type_css)].map(_.size)
+      if(_.min(checks) == 0) {
+        console.error("!!! NO SEARCH FORM FOUND !!!");
+        return setTimeout(doSearch, 5000, options);
+      }
+    }
+    catch(e) {
+      console.error("error on page load: " + e);
+      return setTimeout(doSearch, 5000, options);
+    }
+    browser
+      .select(assesment_year_css, assesment_year)
+      .select(county_code_css, county_code)
+      .select(property_type_css, property_type)
+      .fill(street_address_css, street_address)
+      .pressButton("Search", function() {
+        browser.wait(function() {
+          try {
+            var detail_links = browser.queryAll(detail_link_css);
+            if(detail_links.length) {
+              var upper = detail_links.length >= 10 ? 10 : detail_links.length
+              for(d in _.range(0, upper)) {
+                var id_number = detail_links[d]._attributes._nodes.href._childNodes[0].__nodeValue.split('?ID=')[1]
+                console.log( [assesment_year, county_code, property_type, street_address, id_number].join(','));
+              }
+            }
+          }
+          catch(e) {
+            console.error("error parsing results: " + e);
+          }
+        });
+          return next();
+      });
+    });
 };
 
 var base_url = "http://www.in.gov/apps/dlgftax/Assessment/AssessmentSearch.aspx";
 
 var assesment_year_css = "html body form#form1 div table tbody tr td select#ddlAssesYear";
-var assesment_years = ['2008', '2009', '2010', '2011', '2012']
+var assesment_years = _.range(2008, 2013).map(String);
 
 var county_code_css = "html body form#form1 div table tbody tr td select#ddlCounty";
-var county_codes = _.range(1,92).map(String);
+var county_codes = _.range(1,93).map(String);
 
 var property_type_css = "html body form#form1 div table tbody tr td select#ddlPropType";
-var property_types = ["1%", "2%", "3%", "4%", "5%", "6%", "7%", "8%"];
+var property_types = ["1%", "2%", "3%", "4%", "5%", "6%", "8%"];
 
 var street_address_css = "html body form#form1 div table tbody tr td input#txtStreet"
 var street_addresses = 'abcdefghijklmnopqrstuvwxyz'.split('');
+street_addresses = ['e']
 
 var search_button_css = "html body form#form1 div table tbody tr td input#btnSearch";
 var download_link_css= "html body form#form1 div a#btnDownLoad"
@@ -28,42 +77,23 @@ var pagnation_table_css = "html body form#form1 div div table#gvResults.datagrid
 
 var detail_link_css = "html body form#form1 div div table#gvResults.datagrid tbody tr td a"
 
-var letter = "a";
-var export_js = "var theForm = document.forms[0]; theForm.__EVENTTARGET = 'btnDownLoad'; theForm.__EVENTARGUMENT = ''; theForm.submit()";
+var queries = []
+for (var sa in street_addresses) {
+  for (var ay in assesment_years) {
+    for (var cc in county_codes) {
+      for (var pt in property_types) {
+        queries.push( { assesment_year: assesment_years[ay],
+                        county_code: county_codes[cc],
+                        property_type: property_types[pt],
+                        street_address: street_addresses[sa] } );
+      }
+    }
+  }
+}
 
-// Visit the search form
-Browser.visit(base_url, { debug: true, waitFor: 5000}, function(e, browser, status) {
-  console.log("errors:" + e);
+console.error("Scraping " + queries.length + " pages");
+function next() {
+  setTimeout(doSearch, 500, queries.pop());
+}
 
-  // Handle prompts
-  browser.onprompt(function(msg) {
-    console.log("prompted");
-    console.log(msg);
-  });
-
-  // Execute a search given the defined parameters, for now, print the detail ID for the first page of results.
-  var street_address = street_addresses[0]
-  var assesment_year = assesment_years[0]
-  var county_code = county_codes[0]
-  console.log(property_types);
-  var property_type = property_types[5]
-  browser
-    .select(assesment_year_css, assesment_year)
-    .select(county_code_css, county_code)
-    .select(property_type_css, property_type)
-    .fill(street_address_css, street_address)
-    .pressButton("Search", function() {
-      console.log("waiting for search results to render");
-      browser.wait(function() {
-        console.log("search results rendered");
-        var detail_links = browser.queryAll(detail_link_css);
-        for(d in _.range(0,9)) {
-          var id_number = detail_links[d]._attributes._nodes.href._childNodes[0].__nodeValue.split('?ID=')[1]
-          console.log( [assesment_year, county_code, property_type, street_address, id_number].join(','));
-        }
-        debugger;
-      });
-
-  });
-
-});
+next();
